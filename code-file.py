04ad6ba9b7663d2e6bb658d563e7e4d4fc176f88
codebysubhan <bsdsf22m030@pucit.edu.pk>
2025-07-02 15:29:56 +0500

    trying to commit

--- code-file.py
+++ code-file.py
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 for i in range(10):
     print('hello from new-branch-2')
 
+=======
+def main():
+    for i in range(10):
+        print('hello world')
+    print('main() executed')
+
+if __name__ == '__main__':
+    main()
+>>>>>>> parent of 0a0d0b5 (code-file modified)
